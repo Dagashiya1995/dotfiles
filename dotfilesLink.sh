ec930959d84dcdc4bce5d243a980bcc59bd08302
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2019-09-19 18:37:09 +0900

    Add some

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig 
+ln -sf ~/dotfiles/.vscode/setting.json ~/Library/Application\ Support/Code/User/setting.json
